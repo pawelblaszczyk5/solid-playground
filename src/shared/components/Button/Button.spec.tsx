2a38549673bdc7fc71905178e3e029c6df96ae59
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-09-26 20:26:43 +0200

    Test husky

--- src/shared/components/Button/Button.spec.tsx
+++ src/shared/components/Button/Button.spec.tsx
@@ -1,7 +1,11 @@
 import { render, screen } from 'solid-testing-library';
 import { Button } from './Button';
 
-test('Button should have default text', () => {
+
+
+
+
+test('Button should have default text haha', () => {
   render(() => <Button onClick={() => {}} />);
 
   expect(screen.getByRole('button', { name: 'Click me' })).toBeInTheDocument();

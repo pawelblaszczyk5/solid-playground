1eeca1ca95b04dd609853c4ec0cbbc0860f5ed6e
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-09-26 20:30:06 +0200

    Remove funny part from test name

--- src/shared/components/Button/Button.spec.tsx
+++ src/shared/components/Button/Button.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from 'solid-testing-library';
 import { Button } from './Button';
 
-test('Button should have default text haha', () => {
+test('Button should have default text', () => {
   render(() => <Button onClick={() => {}} />);
 
   expect(screen.getByRole('button', { name: 'Click me' })).toBeInTheDocument();
